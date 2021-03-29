4978e06f79faebed92343a899f82e9d061078560
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2021-03-29 23:44:57 +0100

    feat: Implemented user login

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { handleInitialData } from '../actions/shared';
-import Dashboard from './Dashboard';
-import NewQuestion from './NewQuestion';
+import Login from './Login';
 
 class App extends Component {
   componentDidMount() {
-    this.props.dispatch(handleInitialData());
+    this.props.handleInitialData();
   }
   render() {
-    return <div>{this.props.loading === true ? null : <NewQuestion />}</div>;
+    const { notLoggedIn } = this.props;
+    return (
+      <Router>
+        <Fragment>
+          <div className='main-container'>
+            <Login notLoggedIn={notLoggedIn} />
+          </div>
+        </Fragment>
+      </Router>
+    );
   }
 }
 
 function mapStateToProps({ authedUser }) {
   return {
-    loading: authedUser === null,
+    notLoggedIn: authedUser === null,
   };
 }
 
-export default connect(mapStateToProps)(App);
+function mapDispatchToProps(dispatch) {
+  return {
+    handleInitialData: () => {
+      dispatch(handleInitialData());
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
