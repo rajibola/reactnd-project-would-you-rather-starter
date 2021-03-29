4866281243d47973aea9de4b761ef62cb5616e34
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2021-03-29 23:28:52 +0100

    feat: Created new question component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
 import Dashboard from './Dashboard';
+import NewQuestion from './NewQuestion';
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
+    return <div>{this.props.loading === true ? null : <NewQuestion />}</div>;
   }
 }
 
---
+++ src/components/NewQuestion.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class NewQuestion extends Component {
+  state = {
+    text: '',
+  };
+
+  handleChange = (e) => {
+    const text = e.target.value;
+    this.setState({ text });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({ text: '' });
+  };
+
+  render() {
+    const { text } = this.state;
+    const questionLeft = 280 - text.length;
+    return (
+      <div>
+        <h3 className='center'>Compose New Question</h3>
+        <form className='new-question' onSubmit={this.handleSubmit}>
+          <textarea
+            placeholder='Enter new question'
+            value={text}
+            onChange={this.handleChange}
+            className='textarea'
+            maxLength={280}
+          />
+          {questionLeft <= 100 && (
+            <div className='question-length'>{questionLeft}</div>
+          )}
+          <button className='btn' type='submit' disabled={text === ''}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NewQuestion;
